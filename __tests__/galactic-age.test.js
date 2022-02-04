609590774386904d42bcd28e20e72b8d6b1994f1
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-04 09:26:51 -0800

    add mercury age test for GalacticAge class and code for good fail

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -1,8 +1,14 @@
 import { GalacticAge } from './../src/js/galactic-age.js'
 
 describe('GalacticAge', () => {
-  test('should create a GalacticAge class that takes an age in earth years', () => {
+
+  test('should create a GalacticAge class that takes an age in Earth years', () => {
     let myAge = new GalacticAge(35);
     expect(myAge.earth).toEqual(35);
+  });
+
+  test('should convert age in Earth years to Mercury years', () => {
+    let myAge = new GalacticAge(35);
+    expect(myAge.mercuryAge()).toEqual(145)
   });
 });
--- src/js/galactic-age.js
+++ src/js/galactic-age.js
@@ -2,4 +2,8 @@ export class GalacticAge {
   constructor(earth) {
     this.earth = earth;
   }
+
+  mercuryAge() {
+
+  }
 }
