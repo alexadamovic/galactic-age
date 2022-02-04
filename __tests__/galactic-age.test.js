6850e8f6e458598bc1fc43d92cfa2824e398a09d
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-04 10:31:47 -0800

    add diet and exercise test for GalacticAge class and code for good fail

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -26,4 +26,11 @@ describe('GalacticAge', () => {
     let myAge = new GalacticAge(35);
     expect(myAge.jupiterAge()).toEqual(2);
   });
+
+  test('should collect information about diet and activity levels for GalacticAge class objects', () => {
+    let myAge = new GalacticAge(35, "Healthy", "Active");
+    expect(myAge.earth).toEqual(35);
+    expect(myAge.diet).toEqual("Healthy")
+    expect(myAge.exercise).toEqual("Active")
+  });
 });
