9a6da208fb9239299bd4a6ec4f41cda96b1433e6
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-04 14:51:57 -0800

    add ESLint code fixes to galactic-age.js

--- src/js/galactic-age.js
+++ src/js/galactic-age.js
@@ -10,33 +10,33 @@ export class GalacticAge {
 
   mercuryLeft() {
     if (this.life > this.earthAge) {
-    return Math.floor(this.life / .24) - this.mercuryAge;
+      return Math.floor(this.life / .24) - this.mercuryAge;
     } else {
-      return this.mercuryAge - Math.floor(this.life / .24) + " extra years. Wow!"
+      return this.mercuryAge - Math.floor(this.life / .24) + " extra years. Wow!";
     }
   }
 
   venusLeft() {
     if (this.life > this.earthAge) {
-    return Math.floor(this.life / .62) - this.venusAge;
+      return Math.floor(this.life / .62) - this.venusAge;
     } else {
-      return this.venusAge - Math.floor(this.life / .62) + " extra years. Nice!"
+      return this.venusAge - Math.floor(this.life / .62) + " extra years. Nice!";
     }
   }
 
   marsLeft() {
     if (this.life > this.earthAge) {
       return Math.floor(this.life / 1.88) - this.marsAge;
-      } else {
-        return this.marsAge - Math.floor(this.life / 1.88) + " extra years. Sweet!"
-      }
+    } else {
+      return this.marsAge - Math.floor(this.life / 1.88) + " extra years. Sweet!";
     }
+  }
 
   jupiterLeft() {
     if (this.life > this.earthAge) {
       return Math.floor(this.life / 11.86) - this.jupiterAge;
-      } else {
-        return this.jupiterAge - Math.floor(this.life / 11.86) + " extra years. Bingbong!"
-      }
+    } else {
+      return this.jupiterAge - Math.floor(this.life / 11.86) + " extra years. Bingbong!";
     }
+  }
 }
